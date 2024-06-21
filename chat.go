273d448aaa24406b6889e7990369887fe9f2c2d2
DeborahY <deborah.yang8@gmail.com>
2024-06-21 13:15:42 +0200

    refactor code

--- chat.go
+++ chat.go
@@ -21,6 +21,12 @@ type Message struct {
 	Kind    string `json:"type"`
 }
 
+/*
+chat gère la requête HTTP pour afficher la page de chat.
+
+Paramètres : - w : l'interface utilisée par un gestionnaire HTTP pour construire une réponse HTTP
+    		 - r : la requête HTTP entrante reçue par le serveur
+*/
 func chat(w http.ResponseWriter, r *http.Request) {
 	if user == nil {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
@@ -37,16 +43,187 @@ func chat(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+/*
+handleError gère les erreurs en enregistrant un message d'erreur 
+personnalisé et en terminant le programme si une erreur est présente.
+
+Paramètres: - err: l'erreur à traiter
+		    - errorMessage: un message selon l'erreur
+*/
+func handleError(err error, errorMessage string) {
+	if err != nil {
+		log.Fatalf("%s: %v", errorMessage, err)
+	}
+}
+
+/*
+sendHistory envoie l'historique des messages au client via une connexion WebSocket.
+
+Paramètres: - c: la connexion WebSocket
+			- messages_for_history: la liste des messages à envoyer
+
+Retour: - error: renvoie une erreur si la sérialisation ou l'envoi échoue
+*/
+func sendHistory(c *websocket.Conn, messages_for_history []JSONMessage) error {
+	initialMessages, err := json.Marshal(messages_for_history)
+	handleError(err, "Error marshalling messages")
+
+	return c.WriteMessage(websocket.TextMessage, initialMessages)
+}
+
+/*
+selectWelcomeMessage sélectionne un message de bienvenue aléatoire selon que l'utilisateur
+se connecte pour la première fois ou non.
+
+Paramètres: - isFirstConnection: un booléen qui indique si c'est la première connexion de 
+              					 l'utilisateur
+
+Retour: - string: un message de bienvenue aléatoire.
+*/
+func selectWelcomeMessage(isFirstConnection bool) string {
+	welcomeMessages_newUser := []string{
+		"Bonjour, heureux de vous rencontrer. Comment puis-je vous aider aujourd'hui ?",
+		"Bienvenue ! En quoi puis-je vous aider ?",
+		"Bonjour, enchanté de faire votre connaissance. Que puis-je faire pour vous ?",
+	}
+
+	welcomeMessages_existingUser := []string{
+		"Bonjour, heureux de vous revoir ! Comment puis-je vous aider aujourd'hui ?",
+		"Bonjour, je suis ravi de vous retrouver ! Comment puis-je vous assister aujourd'hui ?",
+		"Bonjour, c'est un plaisir de vous revoir ! En quoi puis-je vous être utile aujourd'hui ?",
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	if isFirstConnection {
+        index := rand.Intn(len(welcomeMessages_newUser))
+		return welcomeMessages_newUser[index]
+	}
+    index := rand.Intn(len(welcomeMessages_existingUser))
+    return welcomeMessages_existingUser[index]
+}
+
+/*
+sendWelcomeMessage envoie un message de bienvenue au client via une connexion WebSocket.
+
+Paramètres: - c: la connexion WebSocket
+			- message: le message de bienvenue à envoyer
+
+Retour: - error: renvoie une erreur si la sérialisation ou l'envoi échoue
+*/
+func sendWelcomeMessage(c *websocket.Conn, message string) error {
+	welcomeMSG, err := json.Marshal(&Message{
+		message,
+		"message",
+	})
+	handleError(err, "Error marshalling message")
+
+	return c.WriteMessage(websocket.TextMessage, welcomeMSG)
+}
+
+/*
+sendMessagesToClient envoie un message au client via une connexion WebSocket.
+
+Paramètres: - c: la connexion WebSocket
+			- messageContent: le contenu du message à envoyer
+			- messageType: le type de message à envoyer
+
+Retour: - error: renvoie une erreur si la sérialisation ou l'envoi échoue
+*/
+func sendMessagesToClient(c *websocket.Conn, messageContent string, messageType string) error {
+	//TODO: check le type du message
+	initialMessages, err := json.Marshal(&Message{
+		messageContent,
+	    messageType,
+	})
+	handleError(err, "Error marshalling message")
+
+	return c.WriteMessage(websocket.TextMessage, initialMessages)
+}
+
+/*
+handleClientCommunication gère l'échange de messages via une connexion WebSocket entre 
+le client et le serveur.
+
+Paramètres: - c: la connexion WebSocket
+			- client: le client qui s'est connecté
+			- messages: une liste contenant les messages envoyés
+
+Valeur de retour: - error: renvoie une erreur si la communication avec le client échoue
+*/
+func handleClientCommunication(c *websocket.Conn, client *api.Client, messages []api.Message) error {
+	pending_msg := ""
+	c.SetReadLimit(5000)
+
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Println("error : ", err)
+			}
+			break
+		}
+
+		message = bytes.TrimSpace(bytes.Replace(message, []byte{'\n'}, []byte{' '}, -1))
+		//log.Printf("rcv : %s\n", message)
+
+		// Sauvegarde des messages dans la base de données
+		userMessage := string(message)
+		date := extract_date_from_message(userMessage)
+		content := extract_content_from_message(userMessage)
+		saveMessage(user.id, "assistant", date, content)
+
+		messages = append(messages, api.Message{
+			Role:    "user",
+			Content: string(message),
+		})
+
+		req := &api.ChatRequest{
+			Model:    "llama3",
+			Messages: messages,
+		}
+
+		err = client.Chat(context.Background(), req, func(m api.ChatResponse) error {
+			err = sendMessagesToClient(c, m.Message.Content, "message")
+			handleError(err, "Error sending message to client")
+
+			pending_msg += m.Message.Content
+
+			if m.Done {
+				current_time := time.Now()
+				formatted_date := current_time.Format("02/01/2006 15:04")
+				saveMessage(user.id, "user", formatted_date, pending_msg)
+
+				messages = append(messages, api.Message{
+					Role:    "assistant",
+					Content: pending_msg,
+				})
+				pending_msg = ""
+
+				err = sendMessagesToClient(c, "#fin#", "message")	
+				handleError(err, "Error sending message to client")
+			}
+
+			return nil
+		})
+
+		handleError(err, "Error sending message to client")
+	}
+
+	return nil
+}
+
+
 var upgrader = websocket.Upgrader{}
 
 /*
-Communication entre les WebSockets
+ws_con gère la communication WebSocket entre le client et le serveur.
+
+Paramètres: - w : l'interface utilisée par un gestionnaire HTTP pour construire une réponse HTTP
+    		- r : la requête HTTP entrante reçue par le serveur
 */
 func ws_con(w http.ResponseWriter, r *http.Request) {
 	client, err := api.ClientFromEnvironment()
-	if err != nil {
-		log.Fatal(err)
-	}
+	handleError(err, "Error getting client")
 
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -61,156 +238,31 @@ func ws_con(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
-	welcomeMessages_newUser := []string{
-		"Bonjour, heureux de vous rencontrer. Comment puis-je vous aider aujourd'hui ?",
-		"Bienvenue ! En quoi puis-je vous aider ?",
-		"Bonjour, enchanté de faire votre connaissance. Que puis-je faire pour vous ?",
-	}
-
-	welcomeMessages_existingUser := []string{
-		"Bonjour, heureux de vous revoir ! Comment puis-je vous aider aujourd'hui ?",
-		"Bonjour, je suis ravi de vous retrouver ! Comment puis-je vous assister aujourd'hui ?",
-		"Bonjour, c'est un plaisir de vous revoir ! En quoi puis-je vous être utile aujourd'hui ?",
-	}
-
 	// Chargement des messages depuis la base de données
 	messages_for_history, err := loadMessageFromDB(user.id)
-	if err != nil {
-		log.Println("Error loading messages:", err)
-		return
-	}
+	handleError(err, "Error loading messages from database")
+	
+	// Envoi des messages de l'historique dans la websocket
+	err = sendHistory(c, messages_for_history)
+	handleError(err, "Error sending history")
 
-	// Envoi des messages de l'historique
-	initialMessages, err := json.Marshal(messages_for_history)
-	if err != nil {
-		log.Println("Error marshalling messages:", err)
-		return
-	}
-	err = c.WriteMessage(websocket.TextMessage, initialMessages)
-	if err != nil {
-		log.Println("Error sending initial messages:", err)
-		return
-	}
-
-	var welcomeMessage string
+	// Check si l'utilisateur se connecte pour la première fois ou non
 	isFirstConnection := false
-	if messages_for_history[0].Content == "New user" {
+	if len(messages_for_history) == 1 && messages_for_history[0].Content == "New user" {
 		isFirstConnection = true
 	}
 
-	if isFirstConnection {
-		rand.Seed(time.Now().UnixNano())
-        index := rand.Intn(len(welcomeMessages_newUser))
-        welcomeMessage = welcomeMessages_newUser[index]
-	} else {
-		rand.Seed(time.Now().UnixNano())
-        index := rand.Intn(len(welcomeMessages_existingUser))
-        welcomeMessage = welcomeMessages_existingUser[index]
-	}
+	// Séléction du message en conséquence et envoie dans la websocket
+	welcomeMessage := selectWelcomeMessage(isFirstConnection)
+	err = sendWelcomeMessage(c, welcomeMessage)
+	handleError(err, "Error sending welcome message")
 
-	log.Println(welcomeMessage)
-	welcomeMSG, err := json.Marshal(&Message{
-		welcomeMessage,
-		"message",
-	})
-	if err != nil {
-		log.Println("Error marshalling messages:", err)
-		return
-	}
-	err = c.WriteMessage(websocket.TextMessage, welcomeMSG)
-	if err != nil {
-		log.Println("Error sending initial messages:", err)
-		return
-	}
 	saveMessage(user.id, "user", time.Now().Format("02/01/2006 15:04"), welcomeMessage)
 
-	end_message, err := json.Marshal(&Message{
-		"#fin#",
-		"message",
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
+	err = sendMessagesToClient(c, "#fin#", "message")	
+	handleError(err, "Error sending message to client")
 
-	err = c.WriteMessage(websocket.TextMessage, end_message)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pending_msg := ""
-	c.SetReadLimit(5000)
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Println("error : ", err)
-			}
-
-			break
-		}
-		message = bytes.TrimSpace(bytes.Replace(message, []byte{'\n'}, []byte{' '}, -1))
-		log.Printf("rcv : %s\n", message)
-
-		// save user message into DB
-		userMessage := string(message)
-		date := extract_date_from_message(userMessage)
-		content := extract_content_from_message(userMessage)
-		saveMessage(user.id, "assistant", date, content)
-
-		messages = append(messages, api.Message{
-			Role:    "user",
-			Content: string(message),
-		})
-		req := &api.ChatRequest{
-			Model:    "llama3",
-			Messages: messages,
-		}
-
-		err = client.Chat(context.Background(), req, func(m api.ChatResponse) error {
-			json_data, err := json.Marshal(&Message{
-				m.Message.Content,
-				"message",
-			})
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			err = c.WriteMessage(mt, json_data)
-			if err != nil {
-				log.Fatal(err)
-			}
-			pending_msg += m.Message.Content
-
-			if m.Done {
-				current_time := time.Now()
-				formatted_date := current_time.Format("02/01/2006 15:04")
-				saveMessage(user.id, "user", formatted_date, pending_msg)
-
-				messages = append(messages, api.Message{
-					Role:    "assistant",
-					Content: pending_msg,
-				})
-				pending_msg = ""
-
-				json_data, err = json.Marshal(&Message{
-					"#fin#",
-					"message",
-				})
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				err = c.WriteMessage(mt, json_data)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-
-			return nil
-		})
-
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	// "début" de la conversation
+	err = handleClientCommunication(c, client, messages)
+	handleError(err, "Error handling client communication")
 }
